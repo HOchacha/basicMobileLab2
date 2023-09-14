31604f731f3d84102ef3d82651abf8d4a20d5034
HOchacha <outcider112@dankook.ac.kr>
2023-09-14 16:08:46 +0900

    feat: /api/history endpoint added

---
+++ src/main/java/org/mse/basicmobilelab2/exception/NoInstanceException.java
@@ -0,0 +1,6 @@
+package org.mse.basicmobilelab2.exception;
+
+public class NoInstanceException extends RuntimeException{
+    public NoInstanceException(){}
+    public NoInstanceException(String message) {super(message);};
+}
